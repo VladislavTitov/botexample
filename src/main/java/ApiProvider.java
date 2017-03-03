9887d8f9db197094c41c9060078f41dd9789c962
nliia <linu28@mail.ru>
2017-03-03 19:20:46 +0300

    reader addeed

--- src/main/java/ApiProvider.java
+++ src/main/java/ApiProvider.java
@@ -1,8 +1,8 @@
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,7 +13,7 @@ import java.nio.file.Paths;
  */
 public class ApiProvider {
     private static String url = "/asr_xml?";
-    private String charset = "UTF-8";
+    private static String charset = "UTF-8";
     private static String apiKey = "8b2415ac-5b77-4b6e-b0e1-3496a8ffbf3c";
     private static String host = "asr.yandex.net";
 
@@ -27,33 +27,29 @@ public class ApiProvider {
 
     static void doPost() throws IOException {
 
-        Path music = Paths.get("C://Users/Ruslan/Downloads/speech.wav");
+        Path music = Paths.get("C://Users/Liia/Downloads/speech.wav");
         byte[] byteArray = Files.readAllBytes(music);
-
         String uuid = "423a423a423a423a423a423a423a423a";
         String urlParameters = "uuid=" + uuid + "&key=" + apiKey + "&topic=numbers&lang=ru-RU";
 
-        URL apiUrl = new URL("https://"+host+url+urlParameters);
+        URL apiUrl = new URL("https://" + host + url + urlParameters);
 
         HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
 
         connection.setRequestMethod("POST");
-        connection.setRequestProperty("Host",host);
-        connection.setRequestProperty("Content-Type","audio/x-speex");
-
-
+        connection.setRequestProperty("Host", host);
+        connection.setRequestProperty("Content-Type", "audio/x-speex");
         connection.setDoOutput(true);
-
-
 
         DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
         wr.writeBytes(String.valueOf(byteArray));
         wr.flush();
         wr.close();
-
-        int responseCode = connection.getResponseCode();
-        System.out.println("ResponseCode: "+responseCode);
-
-
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                connection.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
     }
 }
